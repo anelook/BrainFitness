6ebec8ea80391348f6ad8d3218fbe576fecfbac9
Anelook <olena.kutsenko@gmail.com>
2018-11-18 13:10:28 +0100

    LC-8: added nice java solution

--- math/StringToInteger(atoi)_8/stringToInteger(atoi).js
+++ math/StringToInteger(atoi)_8/stringToInteger(atoi).js
@@ -105,3 +105,29 @@ var myAtoi = function(str) {
     if (res < MIN_INT) return MIN_INT;
     return +res;
 };
+
+
+/** Java solution I liked
+ * https://leetcode.com/problems/string-to-integer-atoi/discuss/4710/My-Nice-Java-Code-3ms
+ * public class Solution {
+	public int myAtoi(String str) {
+		if (str.isEmpty())
+			return 0;
+		str = str.trim();
+		int i = 0, ans = 0, sign = 1, len = str.length();
+		if (str.charAt(i) == '-' || str.charAt(i) == '+')
+			sign = str.charAt(i++) == '+' ? 1 : -1;
+		for (; i < len; ++i) {
+			int tmp = str.charAt(i) - '0';
+			if (tmp < 0 || tmp > 9)
+				break;
+			if (ans > Integer.MAX_VALUE / 10
+					|| (ans == Integer.MAX_VALUE / 10 && Integer.MAX_VALUE % 10 < tmp))
+				return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+			else
+				ans = ans * 10 + tmp;
+		}
+		return sign * ans;
+	}
+}
+ * */
