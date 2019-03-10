6a231fa5a84d0121c33aac17f6af4a05e7ac72ec
Anelook <olena.kutsenko@gmail.com>
2019-03-10 17:11:56 +0100

    LC-141: Java

---
+++ linkedList/Linked List Cycle_141/Linked List Cycle.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+ //Time complexity : O(n)
+ //Space complexity : O(1)
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next && fast.next.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
