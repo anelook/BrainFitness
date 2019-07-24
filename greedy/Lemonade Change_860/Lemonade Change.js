6636d4003fa625273d2bb80eb9c062094bae7b53
Anelook <olena.kutsenko@gmail.com>
2019-07-24 09:21:03 +0200

    LC-860

---
+++ greedy/Lemonade Change_860/Lemonade Change.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+    let fives = 0
+    let tens = 0
+
+    for (let i = 0; i < bills.length; i++) {
+        let bill = bills[i]
+        if (bill === 5) {
+            fives++
+            continue
+        }
+        else if (bill === 10) {
+            fives--
+            tens++
+        }
+        else if (bill === 20) {
+            if (tens > 0) {
+                tens--
+                fives--
+            } else {
+                fives -= 3
+            }
+        }
+        if (fives < 0) {
+            return false
+        }
+    }
+    return true
+}
